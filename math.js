d85cf263802812ecb1154fa88eb0695b56da2764
Nik Stires <nik.stires@gmail.com>
2022-05-21 19:48:26 -0700

    rough draft of math game inplemented

--- math.js
+++ math.js
@@ -1,17 +1,32 @@
 let operations = ['+', '-', '*', '/'];
 let realAnswer = 0;
 let score = 0;
+let count = 0;
+let num1 = 0;
+let num2 = 0;
+let operation = '';
 
-for (let i = 0; i < 5; i++) {
+let inputForm = document.getElementById('math-form');
+let startButton = document.getElementById('start-button');
+let displayQuestion = document.getElementById('math-problem');
+let answerInput = document.getElementById('answer');
+let scoreDisplay = document.getElementById('score');
+
+function AskQuestion() {
   //get first number
-  let num1 = Math.floor(Math.random() * 100);
+  num1 = Math.floor(Math.random() * 10);
   //get second number
-  let num2 = Math.floor(Math.random() * 100);
+  num2 = Math.floor(Math.random() * 10);
   //get random operation
-  let operation = operations[Math.floor(Math.random() * operations.length)];
-  //get answer
-  let answer = prompt('what is ' + num1 + operation + num2);
+  operation = operations[Math.floor(Math.random() * operations.length)];
+  //display question
+  displayQuestion.innerHTML = 'what is ' + num1 + operation + num2;
 
+  count++;
+}
+
+function CheckAnswer(answer) {
+  //calculate correct answer
   switch (operation) {
     case '+':
       realAnswer = num1 + num2;
@@ -26,13 +41,37 @@ for (let i = 0; i < 5; i++) {
       realAnswer = num1 / num2;
   }
 
+  //compare and score accordingly
   if (answer == realAnswer) {
-    console.log('Correct!');
+    scoreDisplay.innerHTML = 'Correct!';
+    scoreDisplay.style.color = 'green';
     score++;
   } else {
-    console.log('Inccorect :(');
+    scoreDisplay.innerHTML = 'Incorrect :(';
+    scoreDisplay.style.color = 'red';
     score--;
+  }
+
+  //ask a new question
+  if (count != 5) {
+    AskQuestion();
+  } else {
+    scoreDisplay.innerHTML = 'You got ' + score + ' points';
+    score >= 1
+      ? (scoreDisplay.style.color = 'green')
+      : (scoreDisplay.style.color = 'red');
   }
 }
 
-console.log('Your score was ' + score);
+startButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  AskQuestion();
+  startButton.style.display = 'none';
+});
+
+answerInput.addEventListener('keypress', function (event) {
+  if (event.keyCode === 13) {
+    CheckAnswer(answerInput.value);
+    answerInput.value = '';
+  }
+});
